87954926d2064a90c4e64d244aad2bec282c0c6b
Marco Apraez <maz_2002atemail.com>
2019-11-02 15:26:57 -0700

    Edit and Index pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,14 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
   def create
     # render plain: params[:article].inspect
     @article = Article.new(article_params)
@@ -19,7 +27,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+
   def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was succesfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
 
