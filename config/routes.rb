809278a0ee6b12844c6b45942e30f52c13fc6723
Marco Apraez <maz_2002atemail.com>
2019-11-09 16:00:15 -0800

    Added user login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
   # post 'users', to: 'users#create'
   resources :users, except: [:new]
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
