ec27c4f7bca8d48b88f53a5a44129259ab0c6f16
Marco Apraez <maz_2002atemail.com>
2019-11-12 05:52:05 -0800

    add signin at signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = (user_params)
       flash[:success] = "Welcome to the Blog Project #{@user.username}"
-      redirect_to articles_path
+      redirect_to users_path(@user)
     else
       render 'new'
     end
